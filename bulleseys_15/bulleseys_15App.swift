460bfebb56404e1e6722ae40c2dfad28ea0603ee
Deelipkumar, Darvesh <deelip.kumar@verizonconnect.com>
2024-01-31 18:53:14 -0500

    Initial Commit

---
+++ bulleseys_15/bulleseys_15App.swift
@@ -0,0 +1,17 @@
+//
+//  bulleseys_15App.swift
+//  bulleseys_15
+//
+//  Created by Deelipkumar Darvesh on 1/31/24.
+//
+
+import SwiftUI
+
+@main
+struct bulleseys_15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ bulleseys_15UITests/bulleseys_15UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  bulleseys_15UITestsLaunchTests.swift
+//  bulleseys_15UITests
+//
+//  Created by Deelipkumar Darvesh on 1/31/24.
+//
+
+import XCTest
+
+final class bulleseys_15UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
